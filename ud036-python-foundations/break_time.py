3a3b4b3b1521909c42e28bb562581f7748cbd5ad
Phil White <pjd.white@gmail.com>
2017-03-31 16:08:07 +0100

    Add extra feature to break_time script

--- ud036-python-foundations/break_time.py
+++ ud036-python-foundations/break_time.py
@@ -1,8 +1,20 @@
+"""
+
+@author: Phil White
+
+Background script to remind the user to take a break from the screen every 2 hours.
+Runs on weekdays, between the hours of 9-5
+"""
+
 import webbrowser
 import time
+import datetime
+date = datetime.date
 
-interval = 0;
-while (interval < 3):
-    time.sleep(10)
-    webbrowser.open('http://www.bbc.co.uk/news')
-    interval = interval + 1
+while True:
+    time.sleep(60 * 60 * 2)
+    today = date.today()
+
+    if today.isoweekday() < 6 and 9 <= time.localtime().tm_hour <= 17:
+        webbrowser.open('http://www.bbc.co.uk/news')
+
